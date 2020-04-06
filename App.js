e83b26b3f1745364386e6eddc2bb64a05ca111a9
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-04-06 14:42:01 +0200

    interactive restaurant menu - application structure in React

---
+++ App.js
@@ -0,0 +1,10 @@
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <div>Menu</div>
+      </>
+    );
+  }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+//Symulacja modułów (w przyszłości będziemy robić) - każdy komponent w innym module (póki co pliku)
+// - index.js - do renderowania
+// - App.js - główny komponent, obsługa stanu
+// - Header.js - nagłówek
+// - ListItems - komponent będący kontenerem na listę potraw
+// - Item.js - komponent na jedną potrawę
+ReactDOM.render(<App />, document.getElementById("root"));
